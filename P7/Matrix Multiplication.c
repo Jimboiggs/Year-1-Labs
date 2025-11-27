32485d6cb2c169cdfec1a87a8f4018c6e830867f
Jimboiggs <jamesworsley07@gmail.com>
2025-11-27 13:18:42 +0000

    Completed matrix multiplication

--- P7/Matrix Multiplication.c
+++ P7/Matrix Multiplication.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
+#include <string.h>
 
 int matrixproduct (int rows1, int columns1, int rows2, int columns2, int matrix1[rows1][columns1], int matrix2[rows2][columns2])
 {
-    int output[rows1][columns2];
+    long int output[rows1][columns2];
     int i;
     int j;
-    int line[columns2]; 
+    int k;
+    int x;
+    int y;
+    long int line[columns2]; 
 
-    if (rows1 < 1 || rows2 < 1 || columns1 != rows2)
+    if (rows1 < 1 || rows2 < 1 || columns1 < 1 || columns2 < 1 || columns1 != rows2)
     {
         printf("Invalid arguments\n");
     }
     else
     {
-        for (i = 0; i < columns1; i++)
+        for (i = 0; i < rows1; i++)
         {
             for (j = 0; j < columns2; j++)
             {
-                line[j] = matrix1[i][j] * matrix2[j][j];
+                int sum = 0;
+                for (k = 0; k < columns1; k++)
+                {
+                    sum += matrix1[i][k] * matrix2[k][j];
+                }
+                line[j] = sum;
             }
-            output[i] = line;
+            
+
+            for (j = 0; j < columns2; j++)
+            {
+                output[i][j] = line[j];
+            }
+
+        }
+
+        for (y = 0; y < rows1; y++)
+        {
+            for (x = 0; x < columns2; x++)
+            {
+                printf("%4li", output[y][x]);
+            }
+            printf("\n");
         }
     }
+    return 0;
 }
