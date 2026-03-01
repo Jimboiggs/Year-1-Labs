3c4973eec5fb6e25712bac8af84a46524c43b2f3
James Worsley <jamesworsley07@gmail.com>
2026-03-01 14:40:07 +0000

    Part 3 draft

--- Java/Lab 4 - Overloading, recursion and exceptions/Recursion and Exception implementation (Part 3)/src/Minimum.java
+++ Java/Lab 4 - Overloading, recursion and exceptions/Recursion and Exception implementation (Part 3)/src/Minimum.java
@@ -1,7 +1,20 @@
-public class Minimum<T> extends Compararble<T> implements TreeMin<T> {
+public class Minimum<T extends Compararble<T>> implements TreeMin<T> {
     @Override
     public T minimum(Node<T> node) {
+        if (node == null) {
+            throw new IllegalArgumentException("Node is null");
+        } else if (node instanceof Leaf<T> leaf) {
+            return leaf.getValue();
+        } else {
+            T leftMin = minimum(branch.getLeft);
+            T rightMin = minimum(branch.getRight());
 
+            if (leftMin.compareTo(rightMin) > 1){
+                return leftMin;
+            } else {
+                return rightMin;
+            }
+        }
     }
 
 }
