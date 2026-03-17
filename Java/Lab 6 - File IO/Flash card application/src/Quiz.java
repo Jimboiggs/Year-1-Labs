8c00e943596443f08691cdf0374b1422fd56995b
Jimboiggs <jamesworsley07@gmail.com>
2026-03-17 17:45:20 +0000

    Part 3 draft

--- Java/Lab 6 - File IO/Flash card application/src/Quiz.java
+++ Java/Lab 6 - File IO/Flash card application/src/Quiz.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Quiz {
     private FlashCardReader cardReader;
     private ArrayList<FlashCard> quizCards;
     private Toolbox toolbox = new Toolbox();
+    private PrintStream ps;
+    private FlashCard card;
+    private String input;
+
+    private boolean correct;
+    int count = 0;
+    int score = 0;
 
     public Quiz(String fileName) {
         cardReader = new FlashCardReader(fileName);
@@ -11,20 +20,51 @@ public class Quiz {
     }
 
     public void play() {
+        String choice = toolbox.readStringFromCmd();
+        if (choice == "Y") {
+            ps = new PrintStream(new File("save.txt"));
+        }
         for (FlashCard card : quizCards) {
+            count++;
             System.out.println(card.getQuestion());
-            String input = toolbox.readStringFromCmd();
+            input = toolbox.readStringFromCmd();
             if (input.equals(card.getAnswer())) {
                 System.out.println("right");
+                correct = true;
+                score++;
             } else {
                 System.out.println("wrong");
                 System.out.println(card.getAnswer());
+                correct = false;
+            }
+            if (choice == "Y") {
+                save();
             }
         }
+        ps.close();
     }
 
     public static void main(String[] args) {
         Quiz quiz = new Quiz("Questions.txt");
         quiz.play();
+    }
+
+    public void save() {
+        ps.print(card.getQuestion());
+        ps.print(",");
+        ps.print(input);
+        ps.print(",");
+        if (correct) {
+            ps.println("right");
+        } else {
+            ps.println("wrong");
+        }
+        if (count == quizCards.size()) {
+            ps.print(score);
+            ps.print(",");
+            ps.print(count);
+            ps.print(",");
+            ps.print(score/count);
+        }
     }
 }
