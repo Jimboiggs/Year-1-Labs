f89d3675a06ba218e84573fde3894f84b705dec3
Jimboiggs <jamesworsley07@gmail.com>
2026-03-31 16:27:09 +0100

    Implemented circular queue logic

--- Java/Lab 7 - Multi-threading and synchronisation/Circular queue/src/CyclicQueue.java
+++ Java/Lab 7 - Multi-threading and synchronisation/Circular queue/src/CyclicQueue.java
@@ -10,4 +10,36 @@ public class CyclicQueue implements NumberQueue {
         this.head = 0;
         this.tail = 0;
     }
+
+    @Override
+    public void enqueue(int number) {
+        if (isFull()) {
+            throw new IllegalStateException("Queue is full");
+        } else {
+            data[head] = number;
+            head = (head + 1) % data.length;
+        }
+    }
+
+    @Override
+    public int dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        int oldTail = tail;
+        tail = (tail + 1) % data.length;
+        return data[oldTail];
+    }
+
+    @Override
+    public boolean isFull()
+    {
+        return (head + 1) % data.length == tail;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return (head == tail);
+    }
 }
