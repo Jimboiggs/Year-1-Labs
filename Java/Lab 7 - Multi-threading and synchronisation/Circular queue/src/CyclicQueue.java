d5560e57517f211723daef335e5382bd3e568ffc
Jimboiggs <jamesworsley07@gmail.com>
2026-03-31 16:07:29 +0100

    Circular queue constructor and variables

--- Java/Lab 7 - Multi-threading and synchronisation/Circular queue/src/CyclicQueue.java
+++ Java/Lab 7 - Multi-threading and synchronisation/Circular queue/src/CyclicQueue.java
@@ -1,2 +1,13 @@
 public class CyclicQueue implements NumberQueue {
+    private int[] data;
+    private int head;
+    private int tail;
+    private int capacity;
+
+    public CyclicQueue(int capacity) {
+        this.capacity = capacity + 1;
+        this.data = new int[this.capacity];
+        this.head = 0;
+        this.tail = 0;
+    }
 }
