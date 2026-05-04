3d7e2a8e1089c3bc1f101f43c23a17e1de162161
Jimboiggs <jamesworsley07@gmail.com>
2026-05-04 18:15:05 +0100

    Complete GameBoard class

--- Java/Lab 8 - Maven and JavaFX/NoughtsAndCrosses/src/main/java/comp1206/GameBoard.java
+++ Java/Lab 8 - Maven and JavaFX/NoughtsAndCrosses/src/main/java/comp1206/GameBoard.java
@@ -25,4 +25,37 @@ public class GameBoard {
             currentPlayer = "X";
         }
     }
+
+    public void makeMove(int row, int col) {
+        if (board[row][col].equals("")) {
+            board[row][col] = currentPlayer;
+        }
+    }
+
+    public String checkWinner() {
+        // Rows and columns
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals("")) {
+                return board[i][0];
+            }
+            else if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals("")) {
+                return board[0][i];
+            }
+        }
+        // Diagonals
+        if (!board[0][0].equals("") &&
+                board[0][0].equals(board[1][1]) &&
+                board[1][1].equals(board[2][2])) {
+
+            return board[0][0];
+        }
+        if (!board[0][2].equals("") &&
+                board[0][2].equals(board[1][1]) &&
+                board[1][1].equals(board[2][0])) {
+
+            return board[0][2];
+        }
+
+        return "";
+    }
 }
